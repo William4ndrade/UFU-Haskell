1d42fd4f825f36d27fffd7d058721f6a1720b535
William Andrade <62865829+William4ndrade@users.noreply.github.com>
2021-07-14 23:24:43 -0300

    Create lab01.hs

---
+++ src/Aula1/lab01.hs
@@ -0,0 +1,39 @@
+
+-- Atividade pratica 14/07/21
+
+
+area_circulo raio = pi * (raio**2)
+
+
+area_trapezio bmenor bmaior altura = ((bmenor + bmaior) * altura) / 2
+
+
+dobro num = num * 2
+
+quadruplicar num = dobro (dobro num)
+
+soma2 x y = x+y
+
+soma4 a b c d = a+b+c+d
+
+misterio a b c d = soma2 (soma2 a b)  (soma2 c d)
+
+hipotenusa catetoA catetob = sqrt ((catetoA**2) + (catetob**2))
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
