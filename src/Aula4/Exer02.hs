e9308d1a062b470928065aafe5728a5e88ca9661
William Andrade <62865829+William4ndrade@users.noreply.github.com>
2021-08-02 15:50:40 -0300

    Chapeuzinho vermelho
    Pela estrada a fora, eu vou bem sozinha
    Levar esses doces para a vovozinha
    Ela mora longe, o caminho é deserto
    E o lobo mau passeia aqui por perto
    Mas à tardinha, ao sol poente
    Junto à mamãezinha dormirei contente

---
+++ src/Aula4/Exer02.hs
@@ -0,0 +1,27 @@
+-- 1
+terceiro :: [Integer] -> Integer  
+terceiro x = head (tail (tail x))
+-----------------------------------------
+
+-- 2 - 
+-- A) -- Ultimo char de uma string
+
+ultimo ::String->Char 
+ultimo x = head (reverse x)
+--ultimo x = last x -- Outra versao
+
+--------------------------------------------------
+-- B) -- String sem o ultimo char
+
+nultimo :: String -> String 
+nultimo x = reverse (tail (reverse x))
+-- nultimo x = init x -- Outra versao
+
+
+---------------------------------------------------
+
+
+-- 3 - Volta as iniciais 
+
+ini :: String -> String -> (Char, Char)
+ini x y = (head x, head y)
